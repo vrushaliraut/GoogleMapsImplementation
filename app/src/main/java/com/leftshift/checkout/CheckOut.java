3e001437b39d1d82eb78e1e4f493cb970bc35a7e
Vrushali <vrushali@leftshift.io>
2015-12-31 16:58:01 +0530

    Chenges in buil.gradel

--- app/src/main/java/com/leftshift/checkout/CheckOut.java
+++ app/src/main/java/com/leftshift/checkout/CheckOut.java
@@ -1,22 +1,104 @@
 package com.leftshift.checkout;
 
-import android.support.v4.app.FragmentActivity;
+import android.app.Activity;
+import android.content.Intent;
+import android.location.Location;
 import android.os.Bundle;
+import android.support.v4.app.FragmentActivity;
+import android.view.View;
+import android.view.ViewTreeObserver;
+import android.widget.Button;
+import android.widget.Toast;
 
+import com.firebase.client.ChildEventListener;
+import com.firebase.client.DataSnapshot;
+import com.firebase.client.Firebase;
+import com.firebase.client.FirebaseError;
+import com.firebase.client.ServerValue;
+import com.google.android.gms.common.GoogleApiAvailability;
+import com.google.android.gms.common.GooglePlayServicesNotAvailableException;
+import com.google.android.gms.common.GooglePlayServicesRepairableException;
+import com.google.android.gms.common.api.GoogleApiClient;
+import com.google.android.gms.common.api.ResultCallback;
+import com.google.android.gms.location.places.Place;
+import com.google.android.gms.location.places.PlaceBuffer;
+import com.google.android.gms.location.places.Places;
+import com.google.android.gms.location.places.ui.PlacePicker;
+import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.SupportMapFragment;
 import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.LatLngBounds;
 import com.google.android.gms.maps.model.MarkerOptions;
 
-public class CheckOut extends FragmentActivity {
+import java.util.HashMap;
+import java.util.Map;
+
+import butterknife.ButterKnife;
+import butterknife.InjectView;
+import timber.log.Timber;
+
+public class CheckOut extends FragmentActivity implements OnMapReadyCallback,ChildEventListener {
 
     private GoogleMap mMap; // Might be null if Google Play services APK is not available.
+    private GoogleApiClient mGoogleApiClient;
+    private LatLngBounds.Builder mBounds = new LatLngBounds.Builder();
+
+    private static final int REQUEST_PLACE_PICKER = 1;
+    @InjectView(R.id.checkout_button)
+    Button checkoutButton;
+
+    private Firebase mFirebase;
+
+    private static final String FIREBASE_URL = "https://glaring-inferno-1670.firebaseio.com/";
+    private static final String FIREBASE_ROOT_NODE = "checkouts";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_check_out);
+
+        ButterKnife.inject(this);
+
+        checkoutButton.getViewTreeObserver().addOnGlobalLayoutListener(
+                new ViewTreeObserver.OnGlobalLayoutListener() {
+                    @Override
+                    public void onGlobalLayout() {
+                        mMap.setPadding(0, checkoutButton.getHeight(), 0, 0);
+                    }
+                }
+        );
+
+        // Set up the API client for Places API
+        mGoogleApiClient = new GoogleApiClient.Builder(this)
+                .addApi(Places.GEO_DATA_API)
+                .build();
+        mGoogleApiClient.connect();
+
+        // Set up Google Maps
+        SupportMapFragment mapFragment = (SupportMapFragment)
+                getSupportFragmentManager().findFragmentById(R.id.map);
+        mapFragment.getMapAsync(this);
+
+        checkoutButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                checkOut(view);
+            }
+        });
+
+
         setUpMapIfNeeded();
+
+        setupFirebase();
+    }
+
+    private void setupFirebase() {
+        // Set up Firebase
+        Firebase.setAndroidContext(this);
+        mFirebase = new Firebase(FIREBASE_URL);
+        mFirebase.child(FIREBASE_ROOT_NODE).addChildEventListener(this);
     }
 
     @Override
@@ -46,11 +128,21 @@ public class CheckOut extends FragmentActivity {
             // Try to obtain the map from the SupportMapFragment.
             mMap = ((SupportMapFragment) getSupportFragmentManager().findFragmentById(R.id.map))
                     .getMap();
+
             // Check if we were successful in obtaining the map.
             if (mMap != null) {
                 setUpMap();
             }
         }
+    }
+
+
+    /**
+     * Map setup. This is called when the GoogleMap is available to manipulate.
+     */
+    @Override
+    public void onMapReady(GoogleMap googleMap) {
+        mMap = googleMap;
     }
 
     /**
@@ -60,6 +152,114 @@ public class CheckOut extends FragmentActivity {
      * This should only be called once and when we are sure that {@link #mMap} is not null.
      */
     private void setUpMap() {
+
         mMap.addMarker(new MarkerOptions().position(new LatLng(0, 0)).title("Marker"));
+        mMap.setMyLocationEnabled(true);
+
+        mMap.setOnMyLocationChangeListener(new GoogleMap.OnMyLocationChangeListener() {
+            @Override
+            public void onMyLocationChange(Location location) {
+                LatLng latitudelongitude = new LatLng(location.getLatitude(), location.getLongitude());
+                Timber.d("latitude:::" + latitudelongitude);
+                addPointToViewPort(latitudelongitude);
+                // we only want to grab the location once, to allow the user to pan and zoom freely.
+                mMap.setOnMyLocationChangeListener(null);
+            }
+
+        });
+
+    }
+
+    /**
+     * Prompt the user to check out of their location. Called when the "Check Out!" button
+     * is clicked.
+     */
+    public void checkOut(View view) {
+        try {
+            PlacePicker.IntentBuilder intentBuilder = new PlacePicker.IntentBuilder();
+            Intent intent = intentBuilder.build(this);
+            startActivityForResult(intent, REQUEST_PLACE_PICKER);
+        } catch (GooglePlayServicesRepairableException e) {
+
+            GoogleApiAvailability.getInstance().getErrorDialog(this, e.getConnectionStatusCode(),
+                    REQUEST_PLACE_PICKER);
+        } catch (GooglePlayServicesNotAvailableException e) {
+
+            Toast.makeText(this, "Please install Google Play Services!", Toast.LENGTH_LONG).show();
+        }
+    }
+
+    private void addPointToViewPort(LatLng newPoint) {
+        mBounds.include(newPoint);
+        mMap.animateCamera(CameraUpdateFactory.newLatLngBounds(mBounds.build(),
+                checkoutButton.getHeight()));
+    }
+
+
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        if (requestCode == REQUEST_PLACE_PICKER) {
+            if (resultCode == Activity.RESULT_OK) {
+
+                Timber.d("data::"+data);
+                Place place = PlacePicker.getPlace(data, this);
+
+                //setp 2 added code
+                Map<String, Object> checkoutData = new HashMap<>();
+                checkoutData.put("time", ServerValue.TIMESTAMP);
+
+                Timber.d("checkoutData:::"+checkoutData);
+
+                mFirebase.child(FIREBASE_ROOT_NODE).child(place.getId()).setValue(checkoutData);
+
+
+            } else if (resultCode == PlacePicker.RESULT_ERROR) {
+
+                Toast.makeText(this, "Places API failure! Check that the API is enabled for your key",
+                        Toast.LENGTH_LONG).show();
+            }
+        } else {
+            super.onActivityResult(requestCode, resultCode, data);
+        }
+    }
+
+    @Override
+    public void onChildAdded(DataSnapshot dataSnapshot, String s) {
+        String placeId = dataSnapshot.getKey();
+        if (placeId != null) {
+            Places.GeoDataApi
+                    .getPlaceById(mGoogleApiClient, placeId)
+                    .setResultCallback(new ResultCallback<PlaceBuffer>() {
+                                           @Override
+                                           public void onResult(PlaceBuffer places) {
+                                               LatLng location = places.get(0).getLatLng();
+                                               addPointToViewPort(location);
+                                               mMap.addMarker(new MarkerOptions().position(location));
+                                               places.release();
+                                           }
+                                       }
+                    );
+        }
+    }
+
+    @Override
+    public void onChildChanged(DataSnapshot dataSnapshot, String s) {
+
+    }
+
+    @Override
+    public void onChildRemoved(DataSnapshot dataSnapshot) {
+
+    }
+
+    @Override
+    public void onChildMoved(DataSnapshot dataSnapshot, String s) {
+
+    }
+
+    @Override
+    public void onCancelled(FirebaseError firebaseError) {
+
     }
 }
